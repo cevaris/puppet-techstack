82432d58ca640502dbe463258d5619d6e810f578
Cevaris <cevaris@gmail.com>
2014-04-28 22:04:22 -0600

    finally was able to login

--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -3,22 +3,60 @@
 #   path    => '/usr/bin'
 # }
 
-node db {
-  include postgresql::server
 
-  postgresql::role {  'sampleuser':
-    password_hash => postgresql_password('sampleuser', 'sample'),
-  }
 
-  postgresql::server::db { 'sampledb':
-    user     => 'sampleuser',
-    password => 'sample'
-  }
 
-  postgresql::server::db { 'sampledb':
-    user     => 'sampleuser',
-    password => 'sample'
-  }
-
+class { "postgresql::server":
+  listen_addresses => "*"
 }
+
+# postgresql::server::db { 'analyze_db':
+#   user     => 'analyze_login',
+#   password => postgresql_password('analyze_login', '3m3rgEncY'),
+# }
+
+
+# class { 'postgresql::server':
+#   ip_mask_allow_all_users => '0.0.0.0/0',
+#   listen_addresses        => '*',
+#   postgres_password       => 'Master_Pass',
+# }
+
+postgresql::server::db { 'simpledb':
+  user     => 'django',
+  password => postgresql_password('django', 'djangopass'),
+}
+
+postgresql::server::pg_hba_rule { 'allow application network to access app database':
+  description => "Open up postgresql for access from 0.0.0.0/0",
+  type => 'host',
+  database => 'simpledb',
+  user => 'django',
+  address => '0.0.0.0/0',
+  auth_method => 'md5',
+}
+
+
+
+# postgresql::server::db { 'test1':
+#   user     => 'test1',
+#   password => 'test1',
+# }
+
+# class { 'postgresql::server':
+#   ip_mask_allow_all_users => '0.0.0.0/0',
+#   listen_addresses        => '*',
+#   postgres_password       => 'masterPass_',
+# }
+# postgresql::server::role { 'sampleuser':
+#   createdb      => true,
+#   login         => true,
+#   password_hash => postgresql_password('sampleuser', 'sample'),
+# }
+# postgresql::server::db { 'sampledb':
+#   user     => 'sampleuser',
+#   password => postgresql_password('sampleuser', 'sample'),
+# }
+
+
 
