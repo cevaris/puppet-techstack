a8164d05c348bc94b6c0baaac2e77fdd0b55f044
Cevaris <cevaris@gmail.com>
2014-05-01 22:57:12 -0600

    working git, permissions, users, groups, and virturalenv

--- puppet/manifests/web.pp
+++ puppet/manifests/web.pp
@@ -3,6 +3,13 @@ exec { 'apt-update':
   path    => '/usr/bin'
 }
 
+$defaults = [ 'vim', 'git']
+package { $defaults: ensure => "installed" }
+
+
+include postgresql::client
+
+
 class { 'python':
   version    => 'system',
   pip        => true,
@@ -10,8 +17,57 @@ class { 'python':
   gunicorn   => true,
 }
 
-vcsrepo { '/var/www/django_sample':
+
+group { 'apps':
+  ensure => "present",
+}
+
+user { 'web':
+    ensure => present,
+    # uid => 2001,
+    gid => 'apps',
+    groups => ['apps'],
+    home => '/home/web',
+    shell => '/bin/bash',
+    password => '$1$IawipmjB$OCWi4dIX15rRxMZ80hRkS/'
+}
+
+# class { 'git': }
+
+
+
+vcsrepo { '/home/web/django_sample':
   ensure   => present,
   provider => git,
   source   => 'https://github.com/cevaris/django-sample',
+  owner    => 'web',
+  group    => 'apps',
+}
+
+# file { '/var/www':
+#   recursive => true,
+#   ensure => "directory",
+#   owner  => "django",
+#   group  => "apps",
+#   mode   => 755,
+# }
+
+
+# python::requirements { '/home/web/django_sample/requirements.txt':
+#   virtualenv => '/home/web/django_sample',
+#   owner      => 'web',
+#   group      => 'apps',
+# }
+
+python::virtualenv { '/home/web/django_sample':
+  ensure       => present,
+  version      => 'system',
+  requirements => '/home/web/django_sample/requirements.txt',
+  # proxy        => 'http://proxy.domain.com:3128',
+  systempkgs   => true,
+  # distribute   => false,
+  # ownero=> 'web',
+  # group        => 'apps',
+  cwd          => '/home/web/django_sample',
+  # timeout      => 0,
 }
