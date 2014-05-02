eb58a8ab786fa600954875e785f5ac9b40429613
Cevaris <cevaris@gmail.com>
2014-05-01 18:56:56 -0600

    got working git repo now

--- puppet/manifests/web.pp
+++ puppet/manifests/web.pp
@@ -1,14 +1,7 @@
-# exec { 'apt-update':
-#   command => 'apt-get update',
-#   path    => '/usr/bin'
-# }
-
-# include git
-
-# class{ 'git':
-#   svn => 'installed',
-#   gui => 'installed',
-# }
+exec { 'apt-update':
+  command => 'apt-get update',
+  path    => '/usr/bin'
+}
 
 class { 'python':
   version    => 'system',
@@ -22,9 +15,3 @@ vcsrepo { '/var/www/django_sample':
   provider => git,
   source   => 'https://github.com/cevaris/django-sample',
 }
-
-
-# git::repo{'repo_name':
-#  path   => '/var/www/django_sample',
-#  source => 'git@github.com:cevaris/django-sample.git'
-# }
