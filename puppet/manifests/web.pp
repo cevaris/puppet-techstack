2be99755704c28b854beac398d57a86625a1aa64
Cevaris <cevaris@gmail.com>
2014-05-03 20:46:53 -0600

    cleaning up web.pp

--- puppet/manifests/web.pp
+++ puppet/manifests/web.pp
@@ -1,56 +1,68 @@
-stage { 'preinstall':
-  before => Stage['main']
-}
+# stage { 'pre':
+#   before => Stage['main']
+# }
 
-class apt_get_update {
-  exec { 'apt-update':
-    command => 'apt-get update',
-    path    => '/usr/bin'
-  }
-}
+# class apt_get_update {
+#   exec { 'apt-update':
+#     command => 'apt-get update',
+#     path    => '/usr/bin'
+#   }
+# }
 
-class { 'apt_get_update':
-  stage => preinstall,
-}
+# class { 'apt_get_update':
+#   stage => pre,
+# }
 
-package { 'git': 
-  ensure => installed 
-}->
+stage { 'pre': before => Stage['main'] }
+class env {
+    include postgresql::client
+    package {
+      'build-essential': ensure => installed;
+      'vim': ensure => installed;
+      'git': ensure => installed;
+    }
+    class { 'python':
+      pip        => true,
+      virtualenv => true,
+      gunicorn   => true,
+      dev        => true,
+    }
+}
+class { "env": stage => 'pre' }
+
+
+
+# Git 
+# package {  }
 vcsrepo { '/var/www/django_sample':
   ensure   => present,
   provider => git,
-  source   => 'https://github.com/cevaris/django-sample',
+  source   => 'https://github.com/cevaris/django-sample', 
+  require  => Package['git'],
 }
 
 
 
-class { 'python':
-  pip        => true,
-  virtualenv => true,
-  gunicorn   => true,
-  dev        => true,
-}->
-# file { ['/root/.pip','/var/www/django_sample/venv','/var/www/django_sample/venv/build']: 
-file { ['/var/www/django_sample/venv','/var/www/django_sample/venv/build']: 
-  ensure => directory,
-  recurse => true,
-}->
-python::virtualenv { '/var/www/django_sample/venv':
-  version      => 'system',  
-  systempkgs   => true,
-  cwd          => '/var/www/django_sample',
-  requirements => '/var/www/django_sample/requirements.txt',
-}->
-python::gunicorn { 'vhost':
-  ensure      => present,
-  virtualenv  => '/var/www/django_sample/venv',
-  mode        => 'wsgi',
-  dir         => '/var/www/django_sample/sample',
-  bind        => 'unix:/tmp/gunicorn.socket',
-  environment => 'prod',
-  template    => 'python/gunicorn.erb',
-}
+# file { ['/var/www/django_sample/venv','/var/www/django_sample/venv/build']: 
+#   ensure => directory,
+#   recurse => true,
+#   require => python_env
+# }->
+# python::virtualenv { '/var/www/django_sample/venv':
+#   version      => 'system',  
+#   systempkgs   => true,
+#   cwd          => '/var/www/django_sample',
+#   requirements => '/var/www/django_sample/requirements.txt',
+# }
+# # ->
+# # python::gunicorn { 'vhost':
+# #   ensure      => present,
+# #   virtualenv  => '/var/www/django_sample/venv',
+# #   mode        => 'wsgi',
+# #   dir         => '/var/www/django_sample/sample',
+# #   bind        => 'unix:/tmp/gunicorn.socket',
+# #   environment => 'prod',
+# #   template    => 'python/gunicorn.erb',
+# # }
 
 
-include postgresql::client
-package { 'vim': ensure => "installed" }
